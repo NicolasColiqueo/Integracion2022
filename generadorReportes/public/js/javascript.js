4f05966ebe753a294fb07fcb5256a848ea9c7680
dalvealinf <dalvaeal2021@alu.uct.cl>
2022-10-13 21:12:08 -0300

    Agregado filtro por columna

--- generadorReportes/public/js/javascript.js
+++ generadorReportes/public/js/javascript.js
@@ -13,13 +13,66 @@ $(document).ready(function () {
         }
     });
 
+    $('#tabla thead tr')
+        .clone(true)
+        .addClass('filters')
+        .appendTo('#tabla thead');
+
     $('#tabla').DataTable({
         "language": {
             "url": "//cdn.datatables.net/plug-ins/1.12.1/i18n/es-ES.json"
         },
         dom: 'lBfrtip',
         buttons: [
-            {extend: 'spacer'}, 'copy', 'csv', 'excel', 'pdf'
-        ]
+            { extend: 'spacer' }, 'copy', 'csv', 'excel', 'pdf'
+        ],
+        orderCellsTop: true,
+        fixedHeader: true,
+        initComplete: function () {
+            var api = this.api();
+
+            api
+                .columns()
+                .eq(0)
+                .each(function (colIdx) {
+                    var cell = $('.filters th').eq(
+                        $(api.column(colIdx).header()).index()
+                    );
+                    var title = $(cell).text();
+                    $(cell).html('<input type="text" placeholder="' + title + '" />');
+
+                    $(
+                        'input',
+                        $('.filters th').eq($(api.column(colIdx).header()).index())
+                    )
+                        .off('keyup change')
+                        .on('change', function (e) {
+                            // Get the search value
+                            $(this).attr('title', $(this).val());
+                            var regexr = '({search})';
+
+                            var cursorPosition = this.selectionStart;
+                            api
+                                .column(colIdx)
+                                .search(
+                                    this.value != ''
+                                        ? regexr.replace('{search}', '(((' + this.value + ')))')
+                                        : '',
+                                    this.value != '',
+                                    this.value == ''
+                                )
+                                .draw();
+                        })
+                        .on('keyup', function (e) {
+                            e.stopPropagation();
+
+                            $(this).trigger('change');
+                            $(this)
+                                .focus()[0]
+                                .setSelectionRange(cursorPosition, cursorPosition);
+                        });
+                });
+        },
     });
+
 });
--- generadorReportes/resources/views/profesor.blade.php
+++ generadorReportes/resources/views/profesor.blade.php
@@ -6,6 +6,7 @@
     <link href="https://cdn.datatables.net/1.12.1/css/dataTables.bootstrap5.min.css" rel="stylesheet">
     <link href="https://cdn.datatables.net/buttons/2.2.3/css/buttons.dataTables.min.css" rel="stylesheet">
     <link href="https://cdn.datatables.net/1.12.1/css/jquery.dataTables.min.css" rel="stylesheet">
+    <link href="https://cdn.datatables.net/fixedheader/3.2.3/css/fixedHeader.dataTables.min.css" rel="stylesheet">
 </head>
 @section('content')
 
@@ -62,6 +63,7 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
 <script src="https://cdn.datatables.net/buttons/2.2.3/js/buttons.html5.min.js"></script>
 <script src="https://cdn.datatables.net/buttons/2.2.3/js/buttons.print.min.js"></script>
+<script src="https://cdn.datatables.net/fixedheader/3.2.3/js/dataTables.fixedHeader.min.js"></script>
 
 @endsection
 
